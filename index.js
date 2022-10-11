a1f30bc91c4b0b328bcbe6b22984f64bc4fd00b9
Josh Polansky <josh@loupe.team>
2022-10-11 13:40:02 -0700

    add connection to a machine

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var widgets = new Widgets("./widgets.json", (data) => {
   widgets.loadPage(data.startPage.container, data.startPage.name)
 })
+
+machine = new WEBHMI.Machine({
+  port: 8000,
+  ipAddress: '127.0.0.1',
+  maxReconnectCount: 5000
+});  
+
