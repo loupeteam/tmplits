49c96518e0a03e8ed33a0b7db23f2938fd3a8f42
Bishop Varnell <bishop@loupe.team>
2023-07-31 14:09:52 -0600

    add a minimum viable AS test project

---
+++ example/AsProject/Logical/Libraries/Loupe/finder/SG4/finder.h
@@ -0,0 +1,76 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+/* finder 0.00.1 */
+
+#ifndef _FINDER_
+#define _FINDER_
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+#ifndef _finder_VERSION
+#define _finder_VERSION 0.00.1
+#endif
+
+#include <bur/plctypes.h>
+
+#ifndef _BUR_PUBLIC
+#define _BUR_PUBLIC
+#endif
+#ifdef _SG3
+		#include "FileIO.h"
+		#include "stringext.h"
+#endif
+#ifdef _SG4
+		#include "FileIO.h"
+		#include "stringext.h"
+#endif
+#ifdef _SGC
+		#include "FileIO.h"
+		#include "stringext.h"
+#endif
+
+
+/* Datatypes and datatypes of function blocks */
+typedef struct finderIn_typ
+{	plcstring filedevice[81];
+	plcstring cwd[241];
+	plcbit refresh;
+} finderIn_typ;
+
+typedef struct finderOut_typ
+{	plcbit done;
+	plcbit updating;
+	unsigned long status;
+} finderOut_typ;
+
+typedef struct finderInternal_typ
+{	plcstring path[241];
+	plcstring cwd[241];
+	signed short state;
+	unsigned long ident;
+	struct DirOpen directoryOpen;
+	struct DirReadEx directoryRead;
+	struct DirClose directoryClose;
+	struct fiDIR_READ_EX_DATA dirData;
+	unsigned long* obj;
+	plcbit comma;
+} finderInternal_typ;
+
+typedef struct finder_typ
+{	struct finderIn_typ in;
+	struct finderOut_typ out;
+	struct finderInternal_typ internal;
+} finder_typ;
+
+
+
+/* Prototyping of functions and function blocks */
+_BUR_PUBLIC unsigned char finder(struct finder_typ* in, unsigned long pBuf, unsigned long sBuf);
+
+
+#ifdef __cplusplus
+};
+#endif
+#endif /* _FINDER_ */
+
---
+++ example/AsProject/Logical/widgetTest/Types.typ
@@ -0,0 +1,26 @@
+
+TYPE
+	widget_typ : 	STRUCT 
+		LabeledLed : BOOL;
+		Led : BOOL;
+		CheckBox : BOOL;
+		NumericInput : REAL;
+		NumericOutput : REAL;
+		LabeledNumericInput : REAL;
+		LableledNumericInput : REAL;
+		LabeledTextInput : STRING[80];
+		LabeledTextOutput : STRING[80];
+		TextOutput : STRING[80];
+		ValueUpDown : UDINT;
+		Slider : REAL;
+		Dropdown : USINT;
+		DropdownTable : USINT;
+		DropdownTableText : STRING[80];
+		TableSelect : USINT;
+		MultiSelect : SINT;
+		MultiSelectText : STRING[80];
+		SetButton : BOOL;
+		ToggleButton : BOOL;
+		MomentaryButton : BOOL;
+	END_STRUCT;
+END_TYPE
