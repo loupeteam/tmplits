7f17e99f3843af0b2515fb78b0aa358aefa7de7c
Bishop Varnell <bishop@loupe.team>
2023-08-07 15:51:56 -0600

    changes to checkbox forgot to commit

--- src/widgets/widgets-checkbox/module.js
+++ src/widgets/widgets-checkbox/module.js
@@ -53,8 +53,7 @@ export function WidgetCheckBox(context, args) {
     return `
     <div class="${classList.join(' ')}" ${attr}>
     <div class='' data-var-name='${dataVarName}' ${attr}></div>
-    <span class="glyphicon glyphicon-check webhmi-show" data-var-name-hide='${dataVarName}' style='font-size:20px;'></span>
-    <span class="glyphicon glyphicon-unchecked webhmi-hide" data-var-name-hide='${dataVarName}' style='font-size:20px;'></span>
+    <span class="glyphicon webhmi-led" data-led-true='glyphicon-check' data-led-false='glyphicon-unchecked' data-var-name='${dataVarName}' style='font-size:20px;'></span>
     ${label}
     </div>
    `
