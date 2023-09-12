5151b1c653d8da183d2ea409db8f079e898e60a1
ashleygprice <ashleyp@autresgrp.com>
2023-09-12 08:07:26 -0700

    Add comments for ColumnBs tmplit

--- src/tmplits/tmplits-columnsbs/module.js
+++ src/tmplits/tmplits-columnsbs/module.js
@@ -1,3 +1,21 @@
+
+/* Example Column (BootStrap) Usage
+    {{#tmplit 'ColumnsBs' 'label' maxColumns=2}}
+        <div style="text-align:center">Column 1 Header</div>
+        <div style="text-align:center">Column 2 Header</div>
+        {{tmplit 'NumericOutput' '<label>' data-var-name='<var>'}}
+        {{tmplit 'NumericOutput' '<label>' data-var-name='<var>'}}
+        {{tmplit 'NumericOutput' '<label>' data-var-name='<var>'}}
+        {{tmplit 'NumericOutput' '<label>' data-var-name='<var>'}}
+    {{/tmplit}}
+
+* Bootstrap divides the page into a 12 columns. This will the maxColumns attribute and check it against the maxColumns. 
+From there it will decide how many columns out of 12 will be created
+* This Tmplit will create the maxColumn amount of columns and add the children elements in order from left to right.
+* The Columns will be equally spaced by the "margin: auto" styling.
+*/
+
+
 import * as util from "../tmplits-utilities/module.js"
 
 export function TmplitColumnsBs(context, args) {
