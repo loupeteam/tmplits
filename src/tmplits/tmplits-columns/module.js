fd9f828188760a68a32c537d49ccf145d34e6dd5
Josh Polansky <josh@loupe.team>
2023-10-03 15:09:19 -0700

    Fix grid typo

--- src/tmplits/tmplits-columns/module.js
+++ src/tmplits/tmplits-columns/module.js
@@ -72,7 +72,7 @@ export function TmplitColumns(context, args) {
         style += `align-items:center;justify-items:center;`
     }
     //Setup columns and rows based on the flow direction
-    style = `${ columnFlow > 0 ? `grid-template: repeat(${rows},1fr) / repeat(${columns},1fr); grid-auto-flow : column;` : `grid-template-columns : repeat(${columns},1fr);`}` + userStyle
+    style = `${ columnFlow > 0 ? `grid-template: repeat(${rows},1fr) / repeat(${columns},1fr); grid-auto-flow : column;` : `grid-template-columns : repeat(${columns},1fr);`}` + style + userStyle
     //This ensures the size is the same but there is a small gap for the border
     // style += `padding: 2px; margin: -2px;`
 
