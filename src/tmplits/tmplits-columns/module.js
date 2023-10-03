c08425be2da09a819eb5581e14bd24c37047ad8c
Josh Polansky <josh@loupe.team>
2023-10-03 14:12:12 -0700

    Add parameters to columns and fix style

--- src/tmplits/tmplits-columns/module.js
+++ src/tmplits/tmplits-columns/module.js
@@ -33,7 +33,7 @@ import * as util from "../tmplits-utilities/module.js"
 export function TmplitColumns(context, args) {
     //Pull out any attributes we need
     let {
-        style = '', centerItems, maxColumns = 3, columnFlow = 0, ..._args
+        style = '', centerItems, maxColumns = 3, minColumns=1, columnFlow = 0, ..._args
     } = args.hash
 
     //Get cleaned values
@@ -60,16 +60,19 @@ export function TmplitColumns(context, args) {
         }
     }
     let columns = count > maxColumns ? maxColumns : count
-    let rows = Math.floor(count / columns)
+    columns = columns < minColumns ? minColumns : columns
 
+    let rows = Math.floor(count / columns)
+    let userStyle = style
     //Generate the style
-    style += `;display:grid; grid-gap:5px;`
+    style = ''
+    style += `display:grid; grid-gap:5px;`
     style += `grid-auto-rows: max-content;`
     if (centerItems) {
         style += `align-items:center;justify-items:center;`
     }
     //Setup columns and rows based on the flow direction
-    style += `${ columnFlow > 0 ? `grid-template: repeat(${rows},1fr) / repeat(${columns},1fr); grid-auto-flow : column;` : `grid-template-columns : repeat(${columns},1fr);`}`
+    style = `${ columnFlow > 0 ? `grid-template: repeat(${rows},1fr) / repeat(${columns},1fr); grid-auto-flow : column;` : `grid-template-columns : repeat(${columns},1fr);`}` + userStyle
     //This ensures the size is the same but there is a small gap for the border
     // style += `padding: 2px; margin: -2px;`
 
