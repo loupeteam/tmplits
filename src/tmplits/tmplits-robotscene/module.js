ef04bf48310901c369c07c091063f4cd96397e16
ryan <ryan@loupe.team>
2023-10-25 15:38:41 -0700

    Add functions to create interactive robot scene

--- src/tmplits/tmplits-robotscene/module.js
+++ src/tmplits/tmplits-robotscene/module.js
@@ -14,15 +14,95 @@
 //}
 
 /* Example RobotScene Usage
-{{tmplit 'RobotScene'}}
+{{tmplit 'RobotScene' robotFile=''}}
 
 Options:
 */
 
 import * as util from "../tmplits-utilities/module.js"
+import * as THREE from './three.module.js'
+import { GLTFLoader } from './GLTFLoader.js'
+import { OrbitControls } from './OrbitControls.js'
+
+let scene, orbit, renderer, camera, canvasHeight, canvasWidth;
+
+window.robotData = {
+	joints: {
+		objects: [],		// populated in loader
+		pose: undefined,	// populated in loader
+		set: undefined		// populated in loader
+	},
+}
+
+function init() {
+
+	canvasHeight = $('#robot').height() - 5;
+	canvasWidth = $('#robot').width() - 5;
+
+	camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 1000);
+	camera.position.set(0, 0, 8);
+	camera.up.set(0, 1, 0)
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setClearColor(0xfcfcfc);
+	renderer.setSize(canvasWidth*2, canvasHeight*2);
+
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+	renderer.domElement.id = 'robotcanvas';
+	renderer.domElement.style.width = canvasWidth + 'px';
+	renderer.domElement.style.height = canvasHeight + 'px';
+
+	// Add light to the scene
+	var hemisphereLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+	hemisphereLight.position.z = 10;
+	scene.add(hemisphereLight);
+
+	// Update camera and renderer settings if the window is resized
+	window.addEventListener('resize', onWindowResize, false);
+	
+	orbit = new OrbitControls(
+		camera,
+		renderer.domElement
+	);
+	orbit.target.set(0, 0, -0.75);
+
+	render()
+}
+
+function onWindowResize() {
+	canvasHeight = $('#robot').height() - 5;
+	canvasWidth = $('#robot').width() - 5;
+
+	camera.aspect = canvasWidth / canvasHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(canvasWidth*2, canvasHeight*2);
+	renderer.domElement.style.width = canvasWidth + 'px';
+	renderer.domElement.style.height = canvasHeight + 'px';
+}
+
+function render() {
+
+	setTimeout(function(){requestAnimationFrame(render);}, 5);
+
+	orbit.update();
+	let el = $('#robot')
+
+	// Render and update stats
+	if(el.is(":visible")){
+		if(el[0].children.length == 0){
+			el[0].appendChild(renderer.domElement);
+		}
+		renderer.render(scene, camera);
+	}
+
+}
 
 export function TmplitRobotScene(context, args) {
     let {
+		['robotFile']:robotFile,
         ..._args
     } = args.hash
     //Get cleaned up values from args
@@ -31,7 +111,34 @@ export function TmplitRobotScene(context, args) {
         attr
     } = util.cleanArgs(_args)
 
+    // Create threeJS scene
+    scene = new THREE.Scene();
+
+    // link glb model files
+    const loader = new GLTFLoader();
+
+    // Initialize the renderer, camera, and scene
+    init();
+
+    loader.load(robotFile, function(gltf) {
+        scene.add(gltf.scene);
+
+        // Load objects
+        robotData.scene = gltf.scene
+
+        scene.traverse(function (object) {
+            if (object.isMesh) {
+                object.castShadow = true
+                object.receiveShadow = true
+            }
+        });
+
+        onWindowResize()
+    }, undefined, function (error) {
+        console.error(error);
+    });
+
     return `
-        <div>"Hello World!"</div>
+        <div id="robot" class="" style="height:600px"></div>
     `
 }
