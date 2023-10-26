acf3a687794e790da626147c47b7cbef75d383f7
ryan <ryan@loupe.team>
2023-10-26 09:52:18 -0700

    Add comments and add loadRobotModel function

--- src/tmplits/tmplits-robotscene/module.js
+++ src/tmplits/tmplits-robotscene/module.js
@@ -14,9 +14,10 @@
 //}
 
 /* Example RobotScene Usage
-{{tmplit 'RobotScene' robotFile=''}}
+{{tmplit 'RobotScene' robotFile='./path/to/robot/file/robot.glb'}}
 
 Options:
+robotFile: gltf file to load into scene
 */
 
 import * as util from "../tmplits-utilities/module.js"
@@ -24,6 +25,7 @@ import * as THREE from './three.module.js'
 import { GLTFLoader } from './GLTFLoader.js'
 import { OrbitControls } from './OrbitControls.js'
 
+// Define global variables
 let scene, orbit, renderer, camera, canvasHeight, canvasWidth;
 
 window.robotData = {
@@ -34,15 +36,20 @@ window.robotData = {
 	},
 }
 
+// Initialize robot scene
 function init() {
+	scene = new THREE.Scene();
 
+	// Define canvas size based on the size of robot scene
 	canvasHeight = $('#robot').height() - 5;
 	canvasWidth = $('#robot').width() - 5;
 
+	// Create and postion camera
 	camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 1000);
 	camera.position.set(0, 0, 8);
 	camera.up.set(0, 1, 0)
 
+	// Create and set up renderer
 	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.setClearColor(0xfcfcfc);
 	renderer.setSize(canvasWidth*2, canvasHeight*2);
@@ -62,15 +69,18 @@ function init() {
 	// Update camera and renderer settings if the window is resized
 	window.addEventListener('resize', onWindowResize, false);
 	
+	// Set up orbit controls for user input
 	orbit = new OrbitControls(
 		camera,
 		renderer.domElement
 	);
 	orbit.target.set(0, 0, -0.75);
 
-	render()
+	// Render the scene
+	render();
 }
 
+// Redefine canvas size based on new size of robot scene
 function onWindowResize() {
 	canvasHeight = $('#robot').height() - 5;
 	canvasWidth = $('#robot').width() - 5;
@@ -83,14 +93,18 @@ function onWindowResize() {
 	renderer.domElement.style.height = canvasHeight + 'px';
 }
 
+// 
 function render() {
 
+	// Perform a request of animation frame every 5 milliseconds
 	setTimeout(function(){requestAnimationFrame(render);}, 5);
 
+	// Update orbit based on user input
 	orbit.update();
-	let el = $('#robot')
 
 	// Render and update stats
+	let el = $('#robot')
+
 	if(el.is(":visible")){
 		if(el[0].children.length == 0){
 			el[0].appendChild(renderer.domElement);
@@ -98,6 +112,30 @@ function render() {
 		renderer.render(scene, camera);
 	}
 
+}
+
+// Handle loading robot gltf file into scene
+function loadRobotModel(robotFile) {
+	const loader = new GLTFLoader();
+
+	loader.load(robotFile, function(gltf) {
+        scene.add(gltf.scene);
+
+        // Load objects
+        robotData.scene = gltf.scene
+
+		// Go through the scene and make sure all objects accurately cast/receive shadows
+        scene.traverse(function (object) {
+            if (object.isMesh) {
+                object.castShadow = true
+                object.receiveShadow = true
+            }
+        });
+
+        onWindowResize()
+    }, undefined, function (error) {
+        console.error(error);
+    });
 }
 
 export function TmplitRobotScene(context, args) {
@@ -111,32 +149,11 @@ export function TmplitRobotScene(context, args) {
         attr
     } = util.cleanArgs(_args)
 
-    // Create threeJS scene
-    scene = new THREE.Scene();
-
-    // link glb model files
-    const loader = new GLTFLoader();
-
     // Initialize the renderer, camera, and scene
     init();
 
-    loader.load(robotFile, function(gltf) {
-        scene.add(gltf.scene);
-
-        // Load objects
-        robotData.scene = gltf.scene
-
-        scene.traverse(function (object) {
-            if (object.isMesh) {
-                object.castShadow = true
-                object.receiveShadow = true
-            }
-        });
-
-        onWindowResize()
-    }, undefined, function (error) {
-        console.error(error);
-    });
+	// Load the robot model into the scene
+	loadRobotModel(robotFile);
 
     return `
         <div id="robot" class="" style="height:600px"></div>
