038d3f1aa03fe128cc194194bc2ea01c29e2ced3
sanhhphan <sanh@loupe.team>
2024-01-09 15:59:05 -0500

    Create skeleton for basic layout web component

--- src/tmplits/tmplits-hmilayout/module.js
+++ src/tmplits/tmplits-hmilayout/module.js
@@ -1,14 +1,25 @@
-//DO NOT DELETE THIS FILE 
-//- Doing so will cause 404 errors on the client side which will not break anything, but will throw errors in the console.
+//Style Template
+const styleTemplate = document.createElement('template');
+styleTemplate.innerHTML = 
+`
+<style>
 
-//This file will get loaded as a javascript module, meaning you can import other modules from here.
-//You can also export functions from here, which will be available to the client side.
+</style>
+`;
 
-//import * from "./module2.js"//Import relative to this file inside node_modules/this-module-name/
-//import * from "../tmplits-some-other/module.js"//Import relative to this file inside node_modules/tmplits-some-other/
-//import * from "/somewhere.js"//Import from the root of the project
+//Web Component Functionality
+class basicLayout extends HTMLElement {
 
-//Define your tmplit functions here and export them to make them globally available
-//export function TmplitHelloWorld(context, args){
-//    return `Hello ${context[0]}!`
-//}
+}
+
+//Register Element (Web Component)
+customElements.define('lui-basicLayout', basicLayout)
+
+
+export function TmplitBasicLayout(context, args) {
+
+}
+
+export function TmplitTabItem(context, args) {
+
+}
