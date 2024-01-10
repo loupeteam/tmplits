89d193ae38033766f5fa05b21322075e32682644
sanhhphan <sanh@loupe.team>
2024-01-10 14:24:25 -0500

    Add basic layout to innerHTML

--- src/tmplits/tmplits-hmilayout/module.js
+++ src/tmplits/tmplits-hmilayout/module.js
@@ -10,16 +10,42 @@ styleTemplate.innerHTML =
 //Web Component Functionality
 class basicLayout extends HTMLElement {
 
+    constructor() {
+
+    
+        this.shadowRoot.innerHTML = `
+        <div class="tmplit-layout-grid">
+            <div class="tmplit-main-container">
+                <div class="tmplit-navBar-container"> 
+                    <div class="tmplit-navBar-logo-wrapper">
+
+                    </div>
+
+                    <div class="tmplit-navBar-buttons-container">
+
+                    </div>
+                </div>
+
+                <div id="tmplit-screen-content" class="tmplit-screen-container"> 
+
+                </div>
+            </div>
+
+            <div class="tmplit-footer-wrapper">
+
+            </div>
+        <div>`
+    }   
 }
 
-//Register Element (Web Component)
-customElements.define('lui-basicLayout', basicLayout)
+// //Register Element (Web Component)
+// customElements.define('lui-basicLayout', basicLayout)
 
 
-export function TmplitBasicLayout(context, args) {
+// export function TmplitBasicLayout1(context, args) {
 
-}
+// }
 
-export function TmplitTabItem(context, args) {
+// export function TmplitBasicLayout2(context, args) {
 
-}
+// }
